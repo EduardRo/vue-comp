3589cb27acdc53622f03b3b14454ef521f2b08dd
EduardRo <eduardstancu@yahoo.com>
2023-05-04 11:41:05 +0300

    sav58 start from begining

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,10 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+<p> from zeroo</p>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
