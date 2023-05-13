94c3f40e37f272a24c8630eeba373424fed6c5be
EduardRo <eduardstancu@yahoo.com>
2023-05-13 15:34:57 +0300

    saveDstrsMyCard.vue

---
+++ src/components/MyCard.vue
@@ -0,0 +1,20 @@
+
+
+<template>
+    <h2>this is the name {{name}}</h2>
+    
+</template>
+<script>
+
+
+export default 
+({
+    name:'Card',
+    data(){
+        return {name:'jhonoson'}
+    }
+    
+    
+})
+</script>
+
